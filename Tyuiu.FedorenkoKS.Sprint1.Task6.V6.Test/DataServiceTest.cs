41e1dad5699f068274174af34188e4f4376bd3ff
Tyuiu.FedorenkoKS@outlook.com <kirilll.fedorenko.2020@mail.ru>
2023-10-18 18:01:01 +0300

    Task6

---
+++ Tyuiu.FedorenkoKS.Sprint1.Task6.V6.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.FedorenkoKS.Sprint1.Task6.V1.Lib;
+
+namespace Tyuiu.FedorenkoKS.Sprint1.Task6.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            string x = "1";
+            string wait = "49";
+            var res = ds.SymbolCode(x);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.FedorenkoKS.Sprint1.Task6.V6/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.FedorenkoKS.Sprint1.Task6.V1.Lib;
+
+namespace Tyuiu.FedorenkoKS.Sprint1.Task6.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Федоренко K.C. | ИСПб-23-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #1                                                              *");
+            Console.WriteLine("* Тема: Работа со строками класс String                                  *");
+            Console.WriteLine("* Задание #6                                                             *");
+            Console.WriteLine("* Вариант #1                                                             *");
+            Console.WriteLine("* Выполнил: Федоренко Кирилл Сергеевич | ИСПб  -23-1                     *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Напишите программу, которая выводит код введенного пользователем       *");
+            Console.WriteLine("* символа                                                                *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+            string x;
+
+            Console.WriteLine("Введите символ и нажмите <ENTER>");
+            Console.WriteLine("Для завершения нажмите точку");
+            x = Console.ReadLine();
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine("Символ: " + x + " Код: " + ds.SymbolCode(x));
+            {
+                ConsoleKeyInfo cons = Console.ReadKey();
+                while (cons.Key != ConsoleKey.Escape)
+                {
+                    cons = Console.ReadKey();
+                }
+            }
+        }
+    }
+}
