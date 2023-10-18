c5c4625fd35d1bd91c1404997840e438f6d20b99
Tyuiu.FedorenkoKS@outlook.com <kirilll.fedorenko.2020@mail.ru>
2023-10-18 18:13:26 +0300

    Task7

---
+++ Tyuiu.FedorenkoKS.Sprint1.Task7.V10.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.FedorenkoKS.Sprint1.Task7.V10.Lib;
+namespace Tyuiu.FedorenkoKS.Sprint1.Task7.V10.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 5;
+            double wait = -1.95;
+            var res = ds.Calculate(x);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.FedorenkoKS.Sprint1.Task7.V10/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.FedorenkoKS.Sprint1.Task7.V10.Lib;
+namespace Tyuiu.FedorenkoKS.Sprint1.Task7.V10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Федоренко К. С. | ИИПб-23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #10                                                             *");
+            Console.WriteLine("* Выполнил: Федоренко К. С. | ИCПб-23-3                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле 2 * ctg(3 * x) - ln cos x / ln(1 + x^2)  *");
+            Console.WriteLine("* и печатает результат на экране.                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double x;
+            Console.Write("Введите значение X: ");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("2 * ctg(3 * x) - ln cos x / ln(1 + x^2) = " + ds.Calculate(x));
+            Console.ReadLine();
+        }
+    }
+}
